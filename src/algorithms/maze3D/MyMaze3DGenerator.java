02016f9942736cf509a3d80045fb929398457e97
Jonathan.P <55094482+Jonathan.P@users.noreply.github.com>
2021-04-06 11:45:52 +0300

    New 3D generator implimantetion

--- src/algorithms/maze3D/MyMaze3DGenerator.java
+++ src/algorithms/maze3D/MyMaze3DGenerator.java
@@ -1,7 +1,5 @@
 package algorithms.maze3D;
-
-import algorithms.mazeGenerators.MyMazeGenerator;
-
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Random;
 
@@ -11,13 +9,55 @@ public class MyMaze3DGenerator extends AMaze3DGenerator {
         //values under 2X2 do not allow us to build a proper maze- exception will be throw
         if (rows < 2 || column < 2 || depth < 1){throw new RuntimeException("Maze must be greater then 1X2X2");}
         Maze3D maze3D = new Maze3D(depth, rows, column);
-        MyMazeGenerator mazeGenerator = new MyMazeGenerator();
         for (int i = 0; i < depth; i++) {
-            maze3D.map[i] = mazeGenerator.generate(rows, column, false).getMaze();
+            generate2D(rows,column,maze3D.map[i]);
         }
         generateStartAndFinish(maze3D);
         return maze3D;
     }
+
+    private void generate2D(int rows,int column, int[][] layer){
+        final ArrayList<int[]> frontiers = new ArrayList<>();
+        final Random random = new Random();
+        int x= ((rows/2));
+        int y= (0);
+        frontiers.add(new int[]{x, y, x, y});
+
+        while (!frontiers.isEmpty()) {
+            final int[] f = frontiers.remove(random.nextInt(frontiers.size()));
+            x = f[2];
+            y = f[3];
+            if (layer[x][y] == 1) {
+                layer[f[0]][f[1]] = layer[x][y] = 0;
+                if (x >= 2 && layer[x - 2][y] == 1)
+                    frontiers.add(new int[]{x - 1, y, x - 2, y});
+                if (y >= 2 && layer[x][y - 2] == 1)
+                    frontiers.add(new int[]{x, y - 1, x, y - 2});
+                if (x < rows - 2 && layer[x + 2][y] == 1)
+                    frontiers.add(new int[]{x + 1 , y, x + 2, y});
+                if (y < column - 2 && layer[x][y + 2] == 1)
+                    frontiers.add(new int[]{x, y + 1 , x, y + 2});
+            }
+        }
+        Random rd = new Random();
+        //fixing the most right
+        for (int i = 0; i < rows - 3 ; i++) {
+            if (layer[i][column - 2] == 0){
+                if (rd.nextBoolean()) { layer[i][column-1] = 0;}
+            }
+        }
+
+        for (int i = 0; i < column - 3 ; i++) {
+            if (layer[1][i] == 0 && layer[0][i] == 1){ //fixing the most up
+                if (rd.nextBoolean()) { layer[0][i] = 0;}
+            }
+
+            if (layer[rows - 2][i] == 0 && layer[rows -1][i] == 1){ //fixing the most bottom
+                if (rd.nextBoolean()) { layer[rows -1][i] = 0;}
+            }
+        }
+    }
+
 
     @Override
     public long measureAlgorithmTimeMillis(int depth, int row, int column) {

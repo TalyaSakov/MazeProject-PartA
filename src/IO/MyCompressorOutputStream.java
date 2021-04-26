d6097a4d1ac0b61fbecc0828aa3b3d964aa45217
Jonathan.P <55094482+Jonathan.P@users.noreply.github.com>
2021-04-26 16:23:45 +0300

    new IO functions

--- src/IO/MyCompressorOutputStream.java
+++ src/IO/MyCompressorOutputStream.java
@@ -1,4 +1,19 @@
 package IO;
 
-public class MyCompressorOutputStream {
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class MyCompressorOutputStream extends OutputStream {
+    OutputStream outputStream;
+
+    public MyCompressorOutputStream(OutputStream outputStream) {
+        this.outputStream = outputStream;
+    }
+
+    @Override
+
+
+    public void write(int b) throws IOException {
+
+    }
 }

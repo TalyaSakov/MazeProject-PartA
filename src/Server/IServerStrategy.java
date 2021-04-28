423372542636e1e6fea36d5bc33ebf375da487cc
Jonathan.P <55094482+Jonathan.P@users.noreply.github.com>
2021-04-28 16:52:18 +0300

    Few updates and new files

--- src/Server/IServerStrategy.java
+++ src/Server/IServerStrategy.java
@@ -1,4 +1,13 @@
 package Server;
 
-public class IServerStrategy {
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+
+public interface IServerStrategy {
+    void applyStrategy(InputStream inFromClient, OutputStream outToClient);
 }
---
+++ src/Server/ServerStrategyGenerateMaze.java
@@ -0,0 +1,39 @@
+package Server;
+import IO.SimpleCompressorOutputStream;
+import algorithms.mazeGenerators.Maze;
+import algorithms.mazeGenerators.MyMazeGenerator;
+
+import java.io.*;
+import java.util.ArrayList;
+
+
+public class ServerStrategyGenerateMaze implements IServerStrategy {
+
+    @Override
+    public void applyStrategy(InputStream inFromClient, OutputStream outToClient) {
+        try {
+            ObjectInputStream fromClient = new ObjectInputStream(inFromClient);
+            ObjectOutputStream toClient = new ObjectOutputStream(outToClient);
+
+            int[] mazeParam = (int [])fromClient.readObject();
+            Maze maze= new MyMazeGenerator().generate(mazeParam[0],mazeParam[1]);
+            byte [] toByte = maze.toByteArray();
+
+//            ByteArrayOutputStream arr=new ByteArrayOutputStream(); //create an input to the stream
+            SimpleCompressorOutputStream arrayToCompress= new SimpleCompressorOutputStream(arr); //sent the arr to the compressor
+
+            //  arrayToCompress.flush();
+            arrayToCompress.write(toByte); //array to write in
+            arrayToCompress.flush();
+            arrayToCompress.close();
+            toClient.writeObject(arr.toByteArray());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
